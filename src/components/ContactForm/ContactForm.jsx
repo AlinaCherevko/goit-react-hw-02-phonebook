9500a2c86efcae50d30a496118d90427f59091e4
Alina Cherevko <alinapavluhina@ukr.net>
2023-12-18 18:20:30 +0200

    Done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,19 +2,31 @@ import React, { Component } from 'react';
 import css from './ContactForm.module.css';
 
 export class ContactForm extends Component {
+  state = {
+    userName: '',
+    userNumber: '',
+  };
+
   formSubmit = e => {
     e.preventDefault();
-
-    const userName = e.currentTarget.elements.name.value;
-    const userNumber = e.currentTarget.elements.number.value;
-
+    const userName = this.state.userName;
+    const userNumber = this.state.userNumber;
+    // const userName = e.currentTarget.elements.name.value;
+    // const userNumber = e.currentTarget.elements.number.value;
     const formData = {
       userName,
       userNumber,
     };
     // console.log(formData);
     this.props.addNewName(formData);
-    e.currentTarget.reset();
+    //контрольовано очищуємо вміст форми:
+    this.setState({ userName: '', userNumber: '' });
+    //очищуємо неконтрольовану форму:
+    // e.currentTarget.reset();
+  };
+  //Напишимо метод що контролює поля форм і змінює стан:
+  onChangeInputForm = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
   };
 
   render() {
@@ -25,11 +37,11 @@ export class ContactForm extends Component {
             Name
           </label>
           <input
+            onChange={this.onChangeInputForm}
+            value={this.state.userName}
             type="text"
-            name="name"
+            name="userName"
             className={css.input}
-            id=""
-            placeholder=""
             required
           />
         </div>
@@ -38,11 +50,11 @@ export class ContactForm extends Component {
             Number
           </label>
           <input
+            onChange={this.onChangeInputForm}
+            value={this.state.userNumber}
             type="number"
-            name="number"
+            name="userNumber"
             className={css.input}
-            id=""
-            placeholder=""
             required
           />
         </div>
